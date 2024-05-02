b71eac0ce892297fd5a04e8d12cfb745c17b6ac3
DESKTOP-VSOD6EN\KOSTA <sebyoug2647@gmail.com>
2024-05-02 17:48:49 +0900

    recipe, like component

--- food-recipe/src/components/Like.jsx
+++ food-recipe/src/components/Like.jsx
@@ -1,5 +1,22 @@
 export default function Like(){
     return (
-        <></>
+        <>
+         {likedFood}
+        </>
     )
 }
+
+const likedFoodList = ["food1","food2","food3","food4","..."]
+
+const likedFood = (
+    <div className="container mx-auto">
+        <ul>
+            {likedFoodList.map((liked,index)=>(
+                <div key={index}>
+                    <li>{liked}</li>
+                    <button>삭제</button>
+                </div>
+            ))}
+        </ul>
+    </div>
+)
